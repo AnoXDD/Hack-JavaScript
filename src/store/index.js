8f5b3e9bdbfa78e3521cac6359ed9c3d83732229
Anoxic <guanrunjie@gmail.com>
2018-02-25 15:39:00 +0800

    Add redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
-import {createStore} from "redux";
+import {applyMiddleware, createStore, compose} from "redux";
+import thunk from "redux-thunk";
+
 import persistedReducer from "../persistor/persistedReducer";
 
+const composeEnhancers = typeof window !== "undefined" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(persistedReducer,
- typeof window !== "undefined" && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  composeEnhancers(applyMiddleware(thunk)));
+
 
 export default store;
